5a630856b657e00b7ad6036ef75947dc1a9b834d
kousik manik <kousik@ambula.app>
2024-12-17 16:59:28 +0530

    fix some issue and added update profile realted api

---
+++ app/routes/updateUserRoute.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const UpdateUserService = require("../controllers/UpdateUserDetailsController"); // Adjust the path as necessary
+const router = express.Router();
+
+// Import the authMiddleware
+const authMiddleware = require("../../middleware/authMiddleware");
+
+// Route to update user details
+router.put("/:userId", authMiddleware, async (req, res) => {
+  const { userId } = req.params;
+  const response = await UpdateUserService.updateUserDetails(userId, req.body);
+  return res.status(response.status).json(response);
+});
+
+// Route to update bank details
+router.put("/:userId/bank-details", authMiddleware, async (req, res) => {
+  const { userId } = req.params;
+  const response = await UpdateUserService.updateBankDetails(userId, req.body);
+  return res.status(response.status).json(response);
+});
+
+// Route to fetch user and bank details
+router.get("/:userId", authMiddleware, async (req, res) => {
+  const { userId } = req.params;
+  const response = await UpdateUserService.getUserAndBankDetails(userId);
+  return res.status(response.status).json(response);
+});
+
+module.exports = router;
