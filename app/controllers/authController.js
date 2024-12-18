f5b9f6b39ec2261ebdc90f08b1e0ec0ee974a476
kousik manik <kousik@ambula.app>
2024-12-19 00:00:23 +0530

    fix register issue and updated referralCode and fix user withdraw request function

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -88,7 +88,7 @@ class AuthController {
         .send(baseResponse.errorResponseWithMessage(error.details[0].message));
     }
 
-    const { referralCode, pin, name, mobileNumber, email, password } = req.body;
+    const { referralCode, pin, name, mobileNumber, email, password } = value;
 
     // Start a database session
     const session = await mongoose.startSession();
@@ -96,38 +96,30 @@ class AuthController {
 
     try {
       // Check for existing user
-      const existingUser = await User.findOne({
-        $or: [{ mobileNumber: value.mobileNumber }, { email: value.email }],
-      });
-      if (existingUser) {
-        logger.warn(
-          "User already exists with provided mobile number or email."
-        );
+      if (await User.exists({ $or: [{ mobileNumber }, { email }] })) {
         throw new Error("User already exists with this mobile number or email");
       }
 
       // Validate referral code
       const referrer = await User.findOne({ referralCode });
-      if (!referrer) {
-        throw new Error("Invalid referral code");
-      }
+      if (!referrer) throw new Error("Invalid referral code");
 
       // Validate pin
       const pinDetails = await PinManagement.findOne({
         pinCode: pin,
         assignedTo: referrer._id,
       });
-      if (!pinDetails || pinDetails.status === "used") {
+      if (!pinDetails || pinDetails.status === "used")
         throw new Error("Invalid or used pin");
-      }
 
-      // Hash the password at the start
-      logger.info("Hashing password.");
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(
+        password,
+        await bcrypt.genSalt(10)
+      );
 
-      // Determine referral parent dynamically
-      let referralParent = await this.getReferralParent(referrer);
+      // Determine referral parent
+      const referralParent = await this.getReferralParent(referrer);
 
       // Create the new user
       const newUser = new User({
@@ -135,25 +127,31 @@ class AuthController {
         mobileNumber,
         email,
         password: hashedPassword,
-        referralCode: this.generateReferralCode(),
+        referralCode: mobileNumber,
         parentReferralCode: referralParent._id,
         status: "active",
       });
 
-      // Save the new user and update referral tracking
+      // Save user and referral tracking
       await newUser.save({ session });
       await ReferralTracking.create(
         [{ referrer: referralParent._id, referred: newUser._id }],
         { session }
       );
 
-      // Update referrer wallet and rank
-      await this.updateReferrerWallet(referralParent._id, pinDetails, session);
+      // Update referral levels and wallet
+      await this.updateReferralLevelsAndWallet(
+        referralParent,
+        referrer,
+        pinDetails,
+        session
+      );
 
+      // Update pin status
       await PinManagement.updateOne(
         { _id: pinDetails._id },
         { $set: { status: "used", updatedAt: new Date() } },
-        { session } // Include session to ensure it's part of the transaction
+        { session }
       );
 
       // Commit transaction
@@ -179,6 +177,56 @@ class AuthController {
     } finally {
       session.endSession();
     }
+  }
+
+  /**
+   * Helper function to update referral levels and wallets.
+   */
+  async updateReferralLevelsAndWallet(
+    referralParent,
+    referrer,
+    pinDetails,
+    session
+  ) {
+    // Recalculate levels
+    const updatedLevelParent = await this.calculateUserLevel(
+      referralParent._id
+    );
+    const updatedLevelReferrer = await this.calculateReferreUserLevel(
+      referrer._id
+    );
+
+    // Update referral parent's level and rank
+    await User.findByIdAndUpdate(
+      referralParent._id,
+      { $set: { level: updatedLevelParent, rank: updatedLevelParent } },
+      { session }
+    );
+
+    logger.info(
+      `Updated referral parent level: ${referralParent._id} to Level ${updatedLevelParent}`
+    );
+
+    // Update referrer's level and rank
+    await User.findByIdAndUpdate(
+      referrer._id,
+      { $set: { level: updatedLevelReferrer, rank: updatedLevelReferrer } },
+      { session }
+    );
+
+    logger.info(
+      `Updated referrer level: ${referrer._id} to Level ${updatedLevelReferrer}`
+    );
+
+    // Update wallet and other metrics
+    await this.updateReferrerWallet(
+      referralParent._id,
+      referrer,
+      pinDetails,
+      updatedLevelReferrer,
+      updatedLevelParent,
+      session
+    );
   }
 
   // Helper Method: Determine referral parent
@@ -215,37 +263,159 @@ class AuthController {
     }
   }
 
-  // Helper Method: Update Referrer Wallet and Rank
-  async updateReferrerWallet(referrerId, pinDetails, session) {
-    const bonusAmount = 10; // Define bonus amount
-    // Fetch the package details using packageId from pinDetails
+  async updateReferrerWallet(
+    referrerId,
+    referral,
+    pinDetails,
+    updatedLevelReferral,
+    updatedLevel,
+    session
+  ) {
     const packageDetails = await PackageModel.findOne({
       _id: pinDetails.packageId,
     });
+    let income = 0;
+    let indirectIncome = 0;
 
-    // Use the package directIncome or fallback to the default bonusAmount
-    const directIncome = packageDetails
-      ? packageDetails.directIncome
-      : bonusAmount;
+    const levelIncomeMap = {
+      60: {
+        1: 4,
+        2: 3,
+        3: 2,
+        4: 1,
+        5: 1,
+        6: 1,
+        7: 1,
+        8: 0.5,
+        9: 0.3,
+        default: 0.2,
+      },
+      200: {
+        1: 10,
+        2: 6,
+        3: 4,
+        4: 1.5,
+        5: 1.5,
+        6: 1.5,
+        7: 1.5,
+        8: 1,
+        9: 0.6,
+        default: 0.4,
+      },
+      1300: {
+        1: 64,
+        2: 48,
+        3: 34,
+        4: 20,
+        5: 20,
+        6: 20,
+        7: 20,
+        8: 10,
+        9: 8,
+        default: 2,
+      },
+    };
 
-    let wallet = await Wallet.findOne({ user: referrerId });
+    const packageIncome = levelIncomeMap[packageDetails.productPrice] || {};
+    income =
+      (packageIncome[updatedLevel] || packageIncome.default) +
+      packageDetails.directIncome;
 
-    if (!wallet) {
-      wallet = new Wallet({
-        user: referrerId,
-        directReferralIncome: directIncome,
-      });
-      await wallet.save({ session });
+    if (referrerId !== referral._id) {
+      indirectIncome =
+        updatedLevel === updatedLevelReferral
+          ? packageIncome[updatedLevelReferral] || packageIncome.default
+          : 0;
+    }
+
+    const updateWallet = async (userId, incomeField, amount) => {
+      const wallet = await Wallet.findOne({ user: userId });
+      if (!wallet) {
+        const newWallet = new Wallet({ user: userId, [incomeField]: amount });
+        await newWallet.save({ session });
+      } else {
+        await Wallet.findOneAndUpdate(
+          { user: userId },
+          {
+            $inc: { [incomeField]: amount },
+            $set: { updatedAt: new Date() },
+          },
+          { session }
+        );
+      }
+    };
+
+    if (referrerId === referral._id) {
+      await updateWallet(referrerId, "directReferralIncome", income);
     } else {
-      await Wallet.findOneAndUpdate(
-        { user: referrerId },
-        {
-          $inc: { directReferralIncome: directIncome },
-          $set: { updatedAt: new Date() },
-        },
-        { session }
+      await updateWallet(
+        referral._id,
+        "indirectReferralIncome",
+        indirectIncome
       );
     }
+  }
+
+  // calculate user level function
+  async calculateUserLevel(userId) {
+    // Fetch direct and indirect referrals in parallel for better performance
+    const [directReferrals, indirectReferrals] = await Promise.all([
+      ReferralTracking.countDocuments({ referrer: userId }),
+      ReferralTracking.countDocuments({
+        referrer: { $ne: userId },
+        referred: userId,
+      }),
+    ]);
+
+    console.log(`${directReferrals} directReferrals`);
+    console.log(`${indirectReferrals} indirectReferrals`);
+
+    const totalCount = directReferrals + indirectReferrals;
+    console.log(`${totalCount} totalCount`);
+
+    // Determine the level based on the thresholds
+    if (totalCount >= 9765625) return 10;
+    if (totalCount >= 1953125) return 9;
+    if (totalCount >= 390625) return 8;
+    if (totalCount >= 78125) return 7;
+    if (totalCount >= 15625) return 6;
+    if (totalCount >= 3125) return 5;
+    if (totalCount >= 625) return 4;
+    if (totalCount >= 125) return 3;
+    if (totalCount >= 25) return 2;
+
+    return 1; // Default level
+  }
+
+  // calculate user referre level
+  async calculateReferreUserLevel(userId) {
+    // Fetch direct and indirect referrals in parallel for better performance
+    const [directReferrals, indirectReferrals] = await Promise.all([
+      ReferralTracking.countDocuments({ referrer: userId }),
+      ReferralTracking.countDocuments({
+        referrer: { $ne: userId },
+        referred: userId,
+      }),
+    ]);
+
+    console.log(`${directReferrals} directReferrals`);
+    console.log(`${indirectReferrals} indirectReferrals`);
+
+    const totalCount = directReferrals + indirectReferrals;
+    console.log(`${totalCount} totalCount`);
+
+    // Determine the level based on the thresholds
+    if (totalCount >= 9765625) return 10;
+    if (totalCount >= 1953125) return 9;
+    if (totalCount >= 390625) return 8;
+    if (totalCount >= 78125) return 7;
+    if (totalCount >= 15625) return 6;
+    if (totalCount >= 3125) return 5;
+    if (totalCount >= 625) return 4;
+    if (totalCount >= 125) return 3;
+    if (totalCount >= 25) return 2;
+
+    return 1; // Default level
   }
 
   // Login Method
--- app/controllers/withDrawController.js
+++ app/controllers/withDrawController.js
@@ -16,6 +16,7 @@ class WithDrawController {
   // Method to create withdrawal request
   async createWithdrawalRequest(req, res) {
     const session = await mongoose.startSession();
+
     try {
       const { error, value } = this.withDrawSchema.validate(req.body);
       if (error) {
@@ -29,39 +30,28 @@ class WithDrawController {
           );
       }
 
-      const { userId, amountRequested } = req.body; // Accept userId and amountRequested in request body
+      const { userId, amountRequested } = value; // Use validated input
 
-      // Start the session
+      // Start the transaction
       session.startTransaction();
 
-      // Find the user's wallet balance
+      // Find the user's wallet
       const wallet = await Wallet.findOne({ user: userId }).session(session);
-
       if (!wallet) {
-        await session.abortTransaction();
-        session.endSession();
-        return res
-          .status(404)
-          .json(baseResponse.errorResponseWithMessage("Wallet not found"));
+        throw new Error("Wallet not found");
       }
 
-      if (wallet.directReferralIncome < 100) {
-        await session.abortTransaction();
-        session.endSession();
-        return res
-          .status(400)
-          .json(
-            baseResponse.errorResponseWithMessage(
-              "Minimum balance of 100 required for withdrawal"
-            )
-          );
+      const totalBalance =
+        wallet.directReferralIncome + wallet.indirectReferralIncome;
+      if (totalBalance < 100) {
+        throw new Error("Minimum balance of 100 required for withdrawal");
       }
 
-      // Deduct 8% from the requested amount for transaction charges
+      // Calculate deductions
       const deductionAmount = amountRequested * 0.08;
       const netAmount = amountRequested - deductionAmount;
 
-      // Create withdrawal request
+      // Create the withdrawal request
       const withdrawalRequest = new WithdrawalRequest({
         user: userId,
         amountRequested,
@@ -73,7 +63,6 @@ class WithDrawController {
 
       // Commit the transaction
       await session.commitTransaction();
-      session.endSession();
 
       logger.info(`Withdrawal request created successfully for user ${userId}`);
 
@@ -90,8 +79,11 @@ class WithDrawController {
       if (session.inTransaction()) {
         await session.abortTransaction();
       }
+      return res
+        .status(500)
+        .json(baseResponse.errorResponseWithMessage(error.message));
+    } finally {
       session.endSession();
-      return res.status(500).json(baseResponse.errorResponse(error));
     }
   }
 
@@ -122,58 +114,51 @@ class WithDrawController {
         .session(session);
 
       if (!withdrawalRequest) {
-        await session.abortTransaction();
-        session.endSession();
-        return res
-          .status(404)
-          .json(
-            baseResponse.errorResponseWithMessage(
-              "Withdrawal request not found"
-            )
-          );
+        throw new Error("Withdrawal request not found");
       }
 
       // Start the session
       session.startTransaction();
 
-      // If approved, deduct the amount from user's wallet
+      // If approved, deduct the amount from the user's wallet
       if (status === "approved") {
         const wallet = await Wallet.findOne({
           user: withdrawalRequest.user._id,
         }).session(session);
 
         if (!wallet) {
-          await session.abortTransaction();
-          session.endSession();
-          return res
-            .status(404)
-            .json(
-              baseResponse.errorResponseWithMessage("User wallet not found")
-            );
+          throw new Error("User wallet not found");
         }
 
-        if (wallet.directReferralIncome < withdrawalRequest.netAmount) {
-          await session.abortTransaction();
-          session.endSession();
-          return res
-            .status(400)
-            .json(
-              baseResponse.errorResponseWithMessage(
-                "Insufficient funds in wallet"
-              )
-            );
+        const totalBalance =
+          wallet.directReferralIncome + wallet.indirectReferralIncome;
+
+        const totalDeductionAmount = withdrawalRequest.amountRequested;
+
+        if (totalBalance < totalDeductionAmount) {
+          throw new Error("Insufficient funds in wallet");
         }
 
-        // Deduct the net amount
-        wallet.directReferralIncome -= withdrawalRequest.netAmount;
+        // Deduct the net amount, first from directReferralIncome, then from indirectReferralIncome if necessary
+        if (wallet.directReferralIncome >= totalDeductionAmount) {
+          wallet.directReferralIncome -= totalDeductionAmount;
+        } else {
+          const remainingAmount =
+            totalDeductionAmount - wallet.directReferralIncome;
+          wallet.directReferralIncome = 0;
+          wallet.indirectReferralIncome -= remainingAmount;
+        }
+
+        // Save the updated wallet
         await wallet.save({ session });
 
+        // Update withdrawal request status and approval date
         withdrawalRequest.status = "approved";
         withdrawalRequest.approvalDate = new Date();
         await withdrawalRequest.save({ session });
 
+        // Commit the transaction
         await session.commitTransaction();
-        session.endSession();
 
         logger.info(
           `Withdrawal request ${withdrawalRequestId} approved successfully for user ${withdrawalRequest.user._id}`
@@ -187,35 +172,17 @@ class WithDrawController {
           );
       }
 
-      // If denied, refund the full requested amount
+      // If denied, no deduction is made, just update the request status
       if (status === "denied") {
-        const wallet = await Wallet.findOne({
-          user: withdrawalRequest.user._id,
-        }).session(session);
-
-        if (!wallet) {
-          await session.abortTransaction();
-          session.endSession();
-          return res
-            .status(404)
-            .json(
-              baseResponse.errorResponseWithMessage("User wallet not found")
-            );
-        }
-
-        // Refund the full requested amount
-        wallet.directReferralIncome += withdrawalRequest.amountRequested;
-        await wallet.save({ session });
-
         withdrawalRequest.status = "denied";
         withdrawalRequest.approvalDate = new Date();
         await withdrawalRequest.save({ session });
 
+        // Commit the transaction
         await session.commitTransaction();
-        session.endSession();
 
         logger.info(
-          `Withdrawal request ${withdrawalRequestId} denied and refunded for user ${withdrawalRequest.user._id}`
+          `Withdrawal request ${withdrawalRequestId} denied for user ${withdrawalRequest.user._id}`
         );
         return res
           .status(200)
@@ -226,6 +193,7 @@ class WithDrawController {
           );
       }
 
+      // Invalid status
       return res
         .status(400)
         .json(baseResponse.errorResponseWithMessage("Invalid status provided"));
@@ -234,8 +202,10 @@ class WithDrawController {
       if (session.inTransaction()) {
         await session.abortTransaction();
       }
-      session.endSession();
       return res.status(500).json(baseResponse.errorResponse(error));
+    } finally {
+      // Always end the session
+      session.endSession();
     }
   }
 }
